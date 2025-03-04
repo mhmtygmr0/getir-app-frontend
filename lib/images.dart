8625970b6d086c8b0a90cf80392ff568796a08c0
Mehmet <mehmetygmrr02@gmail.com>
2025-03-04 14:40:18 +0300

    0

---
+++ lib/images.dart
@@ -0,0 +1,23 @@
+class Images {
+  static final String home = "assets/navigationBarIcons/home.ico";
+  static final String search = "assets/navigationBarIcons/search.ico";
+  static final String person = "assets/navigationBarIcons/person.ico";
+  static final String gift = "assets/navigationBarIcons/gift.ico";
+  static final String drinks = "assets/categoryImages/drinks.png";
+  static final String snacks = "assets/categoryImages/snacks.png";
+  static final String fruits_vegetables =
+      "assets/categoryImages/fruits_vegetables.png";
+  static final String milk_dairy = "assets/categoryImages/milk_dairy.png";
+  static final String baked_goods = "assets/categoryImages/baked_goods.png";
+  static final String ice_cream = "assets/categoryImages/ice_cream.png";
+  static final String food = "assets/categoryImages/food.png";
+  static final String breakfast = "assets/categoryImages/breakfast.png";
+  static final String ready_food = "assets/categoryImages/readyFood.png";
+  static final String meats = "assets/categoryImages/meats.png";
+  static final String fit_form = "assets/categoryImages/fitForm.png";
+  static final String personal_care = "assets/categoryImages/personalCare.png";
+  static final String home_care = "assets/categoryImages/homeCare.png";
+  static final String home_life = "assets/categoryImages/homeLife.png";
+  static final String pets = "assets/categoryImages/pets.png";
+  static final String baby = "assets/categoryImages/baby.png";
+}
--- lib/product/custom_widgets.dart
+++ lib/product/custom_widgets.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../images.dart';
+
 class CustomWidgets {
   static const Color getirColor = Color(0xFF5C3CBB);
-  static final String _gift = "assets/navigationBarIcons/gift.ico";
-  static final String _home = "assets/navigationBarIcons/home.ico";
-  static final String _person = "assets/navigationBarIcons/person.ico";
-  static final String _search = "assets/navigationBarIcons/search.ico";
 
   static PreferredSizeWidget getAppBar(
     String title,
@@ -36,7 +34,7 @@ class CustomWidgets {
           icon: Padding(
             padding: const EdgeInsets.only(top: 8),
             child: Image.asset(
-              _home,
+              Images.home,
               width: 30,
               height: 30,
               color: currentIndex == 0 ? getirColor : Colors.grey,
@@ -48,7 +46,7 @@ class CustomWidgets {
           icon: Padding(
             padding: const EdgeInsets.only(top: 8),
             child: Image.asset(
-              _search,
+              Images.search,
               width: 30,
               height: 30,
               color: currentIndex == 1 ? getirColor : Colors.grey,
@@ -60,7 +58,7 @@ class CustomWidgets {
           icon: Padding(
             padding: const EdgeInsets.only(top: 8),
             child: Image.asset(
-              _person,
+              Images.person,
               width: 25,
               height: 25,
               color: currentIndex == 2 ? getirColor : Colors.grey,
@@ -72,7 +70,7 @@ class CustomWidgets {
           icon: Padding(
             padding: const EdgeInsets.only(top: 8),
             child: Image.asset(
-              _gift,
+              Images.gift,
               width: 25,
               height: 25,
               color: currentIndex == 3 ? getirColor : Colors.grey,
@@ -87,27 +85,21 @@ class CustomWidgets {
   }
 
   static final List<Map<String, String>> categories = [
-    {"image": "assets/categoryImages/drinks.png", "title": "Su & İçecek"},
-    {"image": "assets/categoryImages/snacks.png", "title": "Atıştırmalık"},
-    {
-      "image": "assets/categoryImages/fruits_vegetables.png",
-      "title": "Meyve & Sebze",
-    },
-    {"image": "assets/categoryImages/milk_dairy.png", "title": "Süt Ürünleri"},
-    {"image": "assets/categoryImages/baked_goods.png", "title": "Fırından"},
-    {"image": "assets/categoryImages/ice_cream.png", "title": "Dondurma"},
-    {"image": "assets/categoryImages/food.png", "title": "Temel Gıda"},
-    {"image": "assets/categoryImages/breakfast.png", "title": "Kahvaltılık"},
-    {"image": "assets/categoryImages/readyFood.png", "title": "Yiyecek"},
-    {"image": "assets/categoryImages/meats.png", "title": "Et, Tavuk & Balık"},
-    {"image": "assets/categoryImages/fitForm.png", "title": "Fit & Form"},
-    {
-      "image": "assets/categoryImages/personalCare.png",
-      "title": "Kişisel Bakım",
-    },
-    {"image": "assets/categoryImages/homeCare.png", "title": "Ev Bakım"},
-    {"image": "assets/categoryImages/homeLife.png", "title": "Ev & Yaşam"},
-    {"image": "assets/categoryImages/pets.png", "title": "Evcil Hayvan"},
-    {"image": "assets/categoryImages/baby.png", "title": "Bebek"},
+    {"image": Images.drinks, "title": "Su & İçecek"},
+    {"image": Images.snacks, "title": "Atıştırmalık"},
+    {"image": Images.fruits_vegetables, "title": "Meyve & Sebze"},
+    {"image": Images.milk_dairy, "title": "Süt Ürünleri"},
+    {"image": Images.baked_goods, "title": "Fırından"},
+    {"image": Images.ice_cream, "title": "Dondurma"},
+    {"image": Images.food, "title": "Temel Gıda"},
+    {"image": Images.breakfast, "title": "Kahvaltılık"},
+    {"image": Images.ready_food, "title": "Yiyecek"},
+    {"image": Images.meats, "title": "Et, Tavuk & Balık"},
+    {"image": Images.fit_form, "title": "Fit & Form"},
+    {"image": Images.personal_care, "title": "Kişisel Bakım"},
+    {"image": Images.home_care, "title": "Ev Bakım"},
+    {"image": Images.home_life, "title": "Ev & Yaşam"},
+    {"image": Images.pets, "title": "Evcil Hayvan"},
+    {"image": Images.baby, "title": "Bebek"},
   ];
 }
