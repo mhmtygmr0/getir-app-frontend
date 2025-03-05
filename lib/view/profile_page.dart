698f40fe9b526df44321619025e08c784c59d967
Mehmet <mehmetygmrr02@gmail.com>
2025-03-05 13:24:03 +0300

    0

--- lib/view/profile_page.dart
+++ lib/view/profile_page.dart
@@ -42,64 +42,68 @@ class _ProfilePageState extends State<ProfilePage> {
 
   Widget _getMenuItems() {
     return ListView.builder(
-        itemCount: menuItems.length,
-        itemBuilder: (context, index) {
-          return buildMenuItem(
-            menuItems[index]["image"]!,
-            menuItems[index]["title"]!,
-            isBoxed: index == 0,
-          );
-        });
+      itemCount: menuItems.length,
+      itemBuilder: (context, index) {
+        return buildMenuItem(
+          menuItems[index]["image"]!,
+          menuItems[index]["title"]!,
+          isBoxed: index == 0,
+        );
+      },
+    );
   }
 
   Widget buildMenuItem(String imagePath, String title, {bool isBoxed = false}) {
-    return Card(
-      color: Colors.white,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.zero,
-      ),
-      child: SizedBox(
-        width: double.infinity,
-        height: isBoxed ? 85 : 50,
-        child: Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15),
-              child: isBoxed
-                  ? Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.grey),
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      height: 45,
-                      child: Image.asset(
+    return Padding(
+      padding: const EdgeInsets.only(left: 5, right: 4, bottom: 5),
+      child: Card(
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: SizedBox(
+          width: double.infinity,
+          height: isBoxed ? 85 : 50,
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: isBoxed
+                    ? Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        height: 45,
+                        child: Image.asset(
+                          imagePath,
+                          color: CustomWidgets.getirColor,
+                        ),
+                      )
+                    : Image.asset(
                         imagePath,
+                        height: 25,
                         color: CustomWidgets.getirColor,
                       ),
-                    )
-                  : Image.asset(
-                      imagePath,
-                      height: 25,
-                      color: CustomWidgets.getirColor,
-                    ),
-            ),
-            Text(
-              title,
-              style: TextStyle(
-                fontSize: 15,
-                fontWeight: FontWeight.w500,
               ),
-            ),
-            Spacer(),
-            Padding(
-              padding: const EdgeInsets.only(right: 15),
-              child: Icon(
-                Icons.arrow_forward_ios_rounded,
-                color: CustomWidgets.getirColor,
-                size: 17,
+              Text(
+                title,
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
-            )
-          ],
+              Spacer(),
+              Padding(
+                padding: const EdgeInsets.only(right: 15),
+                child: Icon(
+                  Icons.arrow_forward_ios_rounded,
+                  color: CustomWidgets.getirColor,
+                  size: 17,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
